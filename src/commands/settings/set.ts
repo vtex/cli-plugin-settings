860c6616855adbb366070f7e6c7be9e3a289ac06
Thiago Veras <thiago.veras.machado@gmail.com>
2021-01-12 14:39:41 -0300

    Settings command

--- src/commands/settings/set.ts
+++ src/commands/settings/set.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { CustomCommand, ColorifyConstants } from 'vtex'
-import appsSettingsSet from '../../modules/set'
-
-export default class SettingsSet extends CustomCommand {
-  static description = 'Sets value to an app setting.'
-
-  static examples = [
-    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex settings set')} vtex.service-example fieldName fieldValue`,
-    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex settings set')} vtex.store enableCriticalCSS true`,
-  ]
-=======
 import { CustomCommand } from 'vtex'
 import appsSettingsSet from '../../modules/set'
 
@@ -17,22 +5,15 @@ export default class SettingsSet extends CustomCommand {
   static description = 'Set app settings'
 
   static examples = ['vtex settings set vtex.service-example fieldName fieldValue']
->>>>>>> 528ff8d (Settings command)
 
   static flags = {
     ...CustomCommand.globalFlags,
   }
 
   static args = [
-<<<<<<< HEAD
-    { name: 'appName', required: true, description: `Name of the app.` },
-    { name: 'field', required: true, description: `Name of the setting.` },
-    { name: 'value', required: true, description: `Value of the setting.` },
-=======
     { name: 'appName', required: true },
     { name: 'field', required: true },
     { name: 'value', required: true },
->>>>>>> 528ff8d (Settings command)
   ]
 
   async run() {
--- src/commands/settings/unset.ts
+++ src/commands/settings/unset.ts
@@ -1,18 +1,18 @@
-import { CustomCommand, ColorifyConstants } from 'vtex'
+import { CustomCommand } from 'vtex'
 import appsSettingsUnset from '../../modules/unset'
 
 export default class SettingsUnset extends CustomCommand {
-  static description = 'Disables the specified app setting.'
+  static description = 'Unset app settings'
 
-  static examples = [`${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex settings unset')} vtex.service-example fieldName`]
+  static examples = ['vtex settings unset vtex.service-example fieldName']
 
   static flags = {
     ...CustomCommand.globalFlags,
   }
 
   static args = [
-    { name: 'appName', required: true, description: `Name of the app.` },
-    { name: 'field', required: true, description: `Name of the setting.` },
+    { name: 'appName', required: true },
+    { name: 'field', required: true },
   ]
 
   async run() {
