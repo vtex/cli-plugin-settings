8be511f4b66e0203caff37d6146b21ca444e2f78
Thiago Veras <thiago.veras.machado@gmail.com>
2021-01-12 14:39:41 -0300

    Settings command

--- src/commands/settings/get.ts
+++ src/commands/settings/get.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { CustomCommand, ColorifyConstants } from 'vtex'
 import appsSettingsGet from '../../modules/index'
 
@@ -7,15 +8,30 @@ export default class SettingsGet extends CustomCommand {
   static aliases = ['settings']
 
   static examples = [`${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex settings get')} vtex.service-example`]
+=======
+import { CustomCommand } from 'vtex'
+import appsSettingsGet from '../../modules/index'
+
+export default class SettingsGet extends CustomCommand {
+  static description = 'Get app settings'
+
+  static aliases = ['settings']
+
+  static examples = ['vtex settings get vtex.service-example']
+>>>>>>> 528ff8d (Settings command)
 
   static flags = {
     ...CustomCommand.globalFlags,
   }
 
+<<<<<<< HEAD
   static args = [
     { name: 'appName', required: true, description: 'Name of the app to check the available settings.' },
     { name: 'field', description: 'Name of the setting.' },
   ]
+=======
+  static args = [{ name: 'appName', required: true }, { name: 'field' }]
+>>>>>>> 528ff8d (Settings command)
 
   async run() {
     const {
