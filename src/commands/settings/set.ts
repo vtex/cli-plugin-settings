28147d5faf995ca5e4d730fa98a7957dac6c03d2
Carolina Menezes <carolinamenezes@hotmail.com.br>
2021-02-25 18:00:04 -0300

    Update description

--- src/commands/settings/set.ts
+++ src/commands/settings/set.ts
@@ -1,19 +1,22 @@
-import { CustomCommand } from 'vtex'
+import { CustomCommand, ColorifyConstants } from 'vtex'
 import appsSettingsSet from '../../modules/set'
 
 export default class SettingsSet extends CustomCommand {
-  static description = 'Set app settings'
+  static description = 'Sets value to an app setting.'
 
-  static examples = ['vtex settings set vtex.service-example fieldName fieldValue']
+  static examples = [
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex settings set')} vtex.service-example fieldName fieldValue`,
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex settings set')} vtex.store enableCriticalCSS true`,
+  ]
 
   static flags = {
     ...CustomCommand.globalFlags,
   }
 
   static args = [
-    { name: 'appName', required: true },
-    { name: 'field', required: true },
-    { name: 'value', required: true },
+    { name: 'appName', required: true, description: `Name of the app.` },
+    { name: 'field', required: true, description: `Name of the setting.` },
+    { name: 'value', required: true, description: `Value of the setting.` },
   ]
 
   async run() {
--- src/commands/settings/unset.ts
+++ src/commands/settings/unset.ts
@@ -1,18 +1,18 @@
-import { CustomCommand } from 'vtex'
+import { CustomCommand, ColorifyConstants } from 'vtex'
 import appsSettingsUnset from '../../modules/unset'
 
 export default class SettingsUnset extends CustomCommand {
-  static description = 'Unset app settings'
+  static description = 'Disables the specified app setting.'
 
-  static examples = ['vtex settings unset vtex.service-example fieldName']
+  static examples = [`${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex settings unset')} vtex.service-example fieldName`]
 
   static flags = {
     ...CustomCommand.globalFlags,
   }
 
   static args = [
-    { name: 'appName', required: true },
-    { name: 'field', required: true },
+    { name: 'appName', required: true, description: `Name of the app.` },
+    { name: 'field', required: true, description: `Name of the setting.` },
   ]
 
   async run() {
